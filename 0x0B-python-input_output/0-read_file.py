4bb9732421527a2efab721a739cbc1bf04a00f8c
root <Baronsmiles>
2024-04-17 19:17:48 -0700

    input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a file-reading function."""
+
+def read_file(filename=""):
+    """Read a UTF-8 text file and print its contents to stdout.
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end='')
+:wq
+
